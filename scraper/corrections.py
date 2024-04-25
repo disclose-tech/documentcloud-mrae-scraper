e949a6e72cb65e9de0cd181283d7bd540f19a1a4
disclose_tech <152188727+disclose-tech@users.noreply.github.com>
2024-04-25 13:40:33 +0200

    Update corrections

--- scraper/corrections.py
+++ scraper/corrections.py
@@ -11,5 +11,23 @@ corrections = {
     "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/kppac_2023_15088_rall4_plu_isle_87signe.pdf": {
         "decision_date": "2024-01-23"
     },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024apaca9.pdf": {
+        "petitioner": "Commune de Carnoules (Var)"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024_04_10_janvry_91_revision_plu_avis_deliberenouvelle-2.pdf": {
+        "petitioner": "Commune de Janvry (Essone)"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/pdl-2024-7644_ac_modif1_plui_loirelongue_49_2024acpdl21.pdf": {
+        "petitioner": "Commune de Loire-Longué (49)"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/pdl-2024-7645_ac_modif2_plui_loirelongue_2024acpdl22.pdf": {
+        "petitioner": "Commune de Loire-Longué (49)"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024apbfc4259-pvarnay-le-ducdelibere.pdf": {
+        "decision_date": "2024-04-16"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024acvl8.pdf": {
+        "petitioner": "Communauté de communes de Beauce Loirétaine (Loiret)"
+    },
     # "url": {"decision_date": ""},
 }
