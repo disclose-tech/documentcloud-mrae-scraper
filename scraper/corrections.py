acfdb0138bc2ef9e4ef1e934fcbd15167ba19031
disclose_tech <152188727+disclose-tech@users.noreply.github.com>
2024-07-22 12:10:18 +0200

    Add corrections for 2022

--- scraper/corrections.py
+++ scraper/corrections.py
@@ -30,4 +30,37 @@ corrections = {
         # "petitioner": "Communauté de communes de Beauce Loirétaine (Loiret)"
     },
     # "url": {"decision_date": ""},
+    # 2022
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/kpp_2022_13134-r_ra2_plui_ventadouregletonsmonedieres_vmeeab-3.pdf": {
+        "project": "Révision allégée n°2 du plan local d'urbanisme intercommunal (PLUi) de la communauté de communes Ventadour-Egletons-Monédières (19)"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/kpp_2022_12246_ms_plu_brignaclaplaine_19_d_mrae_signe-2.pdf": {
+        "project": "Modification simplifiée n°1 du plan local d'urbanisme (PLU) de la commune de Brignac-la-Plaine (19)",
+        "title": "Lettre de réponse du 17 juin 2022 au recours de la commune de Brignac-la-Plaine - n° garance : 12246-R",
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/lettre_reponse_rejet_recours_gracieux_kpp_2022_12246_mrae_signe-3.pdf": {
+        "project": "Modification simplifiée n°1 du plan local d'urbanisme (PLU) de la commune de Brignac-la-Plaine (19)",
+        "title": "Décision 2022DKNA58 abrogée",
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/courrier_de_saisine-3.pdf": {
+        "title": "Saisine de la commune de Bernes-sur-Oise"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/courrier_de_saisine-2.pdf": {
+        "title": "Saisine de la commune d'Aulnay-sous-Bois"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/saisine_cacp.pdf": {
+        "title": "Saisine de la Communauté d'Agglomération de Cergy-Pontoise"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/lettre_reponse_rejet_recours_gracieux_kpp_2022_12246_mrae_signe-3-2.pdf": {
+        "title": "Lettre de réponse du 17 juin 2022 au recours de la commune de Brignac-la-Plaine - n° garance : 12246-R"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/demande_formulee_par_l_ept_grand_orly_seine_bievre.pdf": {
+        "title": "Demande du président de l'EPT Grand-Orly Seine Bièvre"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/demande_de_la_prefete_du_val-de-marne.pdf": {
+        "title": "Demande de la préfète du Val-de-Marne"
+    },
+    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/demande_de_la_commune_de_croissy-sur-seine.pdf": {
+        "title": "Saisine de la ville de Croissy sur Seine"
+    },
 }
