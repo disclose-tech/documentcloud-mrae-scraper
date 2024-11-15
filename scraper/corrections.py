7bb9e31892a79736eae80d7484c14ab8fb550500
disclose_tech <152188727+disclose-tech@users.noreply.github.com>
2024-11-15 12:15:09 +0100

    Update corrections

--- scraper/corrections.py
+++ scraper/corrections.py
@@ -1,35 +1,6 @@
 """Hardcoded corrections to prevent manually correcting errors each time we rescrape."""
 
 corrections = {
-    # 2024
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/pp-2023-15189-ra3-plu-saintjeand_angely_17_.pdf": {
-        # "decision_date": "2024-03-22"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2023abfcxx_rev_simpl_seloncourt-25-flashdelibere.pdf": {
-        # "decision_date": "2024-01-09"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/kppac_2023_15088_rall4_plu_isle_87signe.pdf": {
-        # "decision_date": "2024-01-23"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024apaca9.pdf": {
-        # "petitioner": "Commune de Carnoules (Var)"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024_04_10_janvry_91_revision_plu_avis_deliberenouvelle-2.pdf": {
-        # "petitioner": "Commune de Janvry (Essone)"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/pdl-2024-7644_ac_modif1_plui_loirelongue_49_2024acpdl21.pdf": {
-        # "petitioner": "Commune de Loire-Longué (49)"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/pdl-2024-7645_ac_modif2_plui_loirelongue_2024acpdl22.pdf": {
-        # "petitioner": "Commune de Loire-Longué (49)"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024apbfc4259-pvarnay-le-ducdelibere.pdf": {
-        # "decision_date": "2024-04-16"
-    },
-    "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/2024acvl8.pdf": {
-        # "petitioner": "Communauté de communes de Beauce Loirétaine (Loiret)"
-    },
-    # "url": {"decision_date": ""},
     # 2022
     "https://www.mrae.developpement-durable.gouv.fr/IMG/pdf/kpp_2022_13134-r_ra2_plui_ventadouregletonsmonedieres_vmeeab-3.pdf": {
         "project": "Révision allégée n°2 du plan local d'urbanisme intercommunal (PLUi) de la communauté de communes Ventadour-Egletons-Monédières (19)"
